b9fe297cd743863525839f2b11b0e8a4dafd1004
Kaique Munhoz <munhoz13km@gmail.com>
2021-05-19 04:51:03 -0300

    feat: Add an array of Items to Meal

--- alura/eggplant-brownie/eggplant-brownie/Models/Meal.swift
+++ alura/eggplant-brownie/eggplant-brownie/Models/Meal.swift
@@ -3,9 +3,10 @@ import Foundation
 class Meal {
     let name: String
     let happiness: Double
+    var items: Array<Item> = []
     
     init(name: String, happiness: Double) {
         self.name = name
         self.happiness = happiness
     }
-}
+    }
