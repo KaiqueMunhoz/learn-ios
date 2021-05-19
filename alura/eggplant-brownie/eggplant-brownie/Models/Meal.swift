d16015cdc19434f8410aafaa15b816e00467740d
Kaique Munhoz <munhoz13km@gmail.com>
2021-05-19 04:37:33 -0300

    feat: Create Meal

---
+++ alura/eggplant-brownie/eggplant-brownie/Models/Meal.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class Meal {
+    let name: String
+    let happiness: Double
+    
+    init(name: String, happiness: Double) {
+        self.name = name
+        self.happiness = happiness
+    }
+}
