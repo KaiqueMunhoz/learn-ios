e7f70c422fe9656a730e1e179363435be9418578
Kaique Munhoz <munhoz13km@gmail.com>
2021-09-09 16:45:56 -0300

    feat: Create Simple app

---
+++ pluralsight/the-big-picture/Simple/Simple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple
+//
+//  Created by Kaique Munhoz on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
